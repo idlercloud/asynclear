9609f9c1c63c8505ae3653c280a3532405dbcc78
cxz888 <idlercloud@gmail.com>
2023-12-29 01:27:25 +0800

    fix(kernel): panic 信息用 println!() 而非 error!() 打印

--- crates/kernel/src/lang_items.rs
+++ crates/kernel/src/lang_items.rs
@@ -2,7 +2,7 @@ use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(info: &PanicInfo<'_>) -> ! {
-    error!("{}", info);
+    println!("{}", info);
     sbi_rt::system_reset(sbi_rt::Shutdown, sbi_rt::SystemFailure);
     unreachable!();
 }
