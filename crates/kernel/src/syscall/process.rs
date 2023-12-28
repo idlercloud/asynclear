596506e6de330210c7f14e8e55440072aeec911f
cxz888 <idlercloud@gmail.com>
2023-12-29 00:56:00 +0800

    chore: 微调文档及更新 Cargo.lock

--- crates/kernel/src/syscall/process.rs
+++ crates/kernel/src/syscall/process.rs
@@ -150,7 +150,7 @@ pub async fn sys_wait4(pid: isize, wstatus: usize, options: usize, rusage: usize
     }
 
     // 尝试寻找符合条件的子进程
-    // TODO: 这里的逻辑感觉可以重写下
+    // TODO: 这里的逻辑感觉可以重写下。process inner 也可以不用设为 pub
     loop {
         {
             let process = curr_process();
