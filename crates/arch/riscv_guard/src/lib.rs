a5adaf45e42296d43cd08aabb092c0dd3a89819b
cxz888 <idlercloud@gmail.com>
2024-01-18 03:37:39 +0800

    feat(riscv_guard): 添加 sum guard

--- crates/arch/riscv_guard/src/lib.rs
+++ crates/arch/riscv_guard/src/lib.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![feature(negative_impls)]
 
+// FIXME: 这些 guard 都要求 guard 的创建和析构呈严格栈结构，否则会有问题，需要改进
+
 use riscv::register::sstatus;
 
 pub struct NoIrqGuard {
@@ -28,6 +30,36 @@ impl Drop for NoIrqGuard {
         if self.before {
             unsafe {
                 sstatus::set_sie();
+            }
+        }
+    }
+}
+
+pub struct AccessUserGuard {
+    before: bool,
+}
+
+// 不允许 Guard 越过 .await
+impl !Send for AccessUserGuard {}
+
+impl AccessUserGuard {
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
+        let before = sstatus::read().sum();
+        if !before {
+            unsafe {
+                sstatus::set_sum();
+            }
+        }
+        Self { before }
+    }
+}
+
+impl Drop for AccessUserGuard {
+    fn drop(&mut self) {
+        if !self.before {
+            unsafe {
+                sstatus::clear_sum();
             }
         }
     }
