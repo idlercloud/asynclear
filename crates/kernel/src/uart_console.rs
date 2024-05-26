1fec246b06549d8ca4e32d4679864eb212ec6a97
cxz888 <idlercloud@gmail.com>
2024-05-26 20:09:54 +0800

    feat: 内核增加 `eprint!()`/`eprintln!()` 用以强制输出

--- crates/kernel/src/uart_console.rs
+++ crates/kernel/src/uart_console.rs
@@ -16,15 +16,24 @@ impl Write for Stdout {
 
 pub static STDOUT: SpinNoIrqMutex<Stdout> = SpinNoIrqMutex::new(Stdout(()));
 
-/// 输出到 stdout
 #[inline]
-fn stdout_puts(fmt: Arguments<'_>) {
-    STDOUT.lock().write_fmt(fmt).unwrap();
+pub fn print(args: Arguments<'_>) {
+    STDOUT.lock().write_fmt(args).unwrap();
 }
 
 #[inline]
-pub fn print(args: Arguments<'_>) {
-    stdout_puts(args);
+pub fn eprint(args: Arguments<'_>) {
+    struct Stderr;
+    impl Write for Stderr {
+        fn write_str(&mut self, s: &str) -> Result {
+            for byte in s.bytes() {
+                #[allow(deprecated)]
+                sbi_rt::legacy::console_putchar(byte as usize);
+            }
+            Ok(())
+        }
+    }
+    let _ = Stderr.write_fmt(args);
 }
 
 /// 打印格式字符串，无换行
@@ -36,8 +45,23 @@ pub macro print {
 
 /// 打印格式字符串，有换行
 pub macro println {
-    () => ($crate::print!("\n")),
+    () => (print!("\n")),
     ($($arg:tt)*) => {
         $crate::uart_console::print(core::format_args_nl!($($arg)*));
+    }
+}
+
+/// 强制打印格式字符串，无换行
+pub macro eprint {
+    ($($arg:tt)*) => {
+        $crate::uart_console::eprint(core::format_args!($($arg)*));
+    }
+}
+
+/// 强制打印格式字符串，有换行
+pub macro eprintln {
+    () => (eprint!("\n")),
+    ($($arg:tt)*) => {
+        $crate::uart_console::eprint(core::format_args_nl!($($arg)*));
     }
 }
